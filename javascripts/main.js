59a94c8c8164350352f83c4e4f4b9db4dd7b5159
lesliemo <theneurotrust@gmail.com>
2016-11-16 11:42:18 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,2 @@
-// Need JavaScript for KNOC.github.io/javascripts/KYGreenBookDirectory.js Google Map API integration
+// Need JavaScript for KNOC.github.io/javascripts/KYGreenBookDirectory-source-code.js Google Map API integration
 // original documentation notation found in this document [console.log('This would be the main JS file.');] -- not sure whether it is needed
